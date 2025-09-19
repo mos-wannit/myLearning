30c80ef54ead61d0d30e0bd875f07dc066f4c649
mosmo <102788478+mos-wannit@users.noreply.github.com>
2025-09-19 14:47:50 +0700

    init

---
+++ src/main/java/com/example/mylearningproject/MyLearningProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.mylearningproject;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyLearningProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mylearningproject/MyLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylearningproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
